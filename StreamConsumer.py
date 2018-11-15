80afb108f68765fbac7909481cde0f054a3b46d2
pranavlal30 <pranav.lal30@gmail.com>
2018-11-14 18:04:20 -0600

    Plugged in VGG model for audio classification

--- StreamConsumer.py
+++ StreamConsumer.py
@@ -3,16 +3,26 @@ from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
 import numpy as np
+from AudioClassifier.parse_file import process_data
+def classify(rdd):
+    wav_data = np.asarray(rdd.take(1), dtype=np.int16)
+    if len(wav_data) > 0:
+        wav_data = wav_data.reshape(220500,2)
+        prediction = process_data(wav_data)
+        print prediction
+
+    
+
+	
 if __name__ == "__main__":
     sc = SparkContext(appName="PythonStreamingDirectKafkaWordCount")
     ssc = StreamingContext(sc, 2)
     brokers = 'localhost:9092'
     topic = 'test2'
     kafkaParams = {"metadata.broker.list": brokers}
-    kvs = KafkaUtils.createDirectStream(ssc, [topic], kafkaParams, valueDecoder=lambda x: x)
-    wav_data = kvs.map(lambda x: np.loads(x[1]))
-    print type(wav_data)
-    print 'asdfsf'
-    wav_data.pprint()
+    audioStream = KafkaUtils.createDirectStream(ssc, [topic], kafkaParams, valueDecoder=lambda x: x)
+    wav_data = audioStream.map(lambda x: np.loads(x[1]))
+    pred = wav_data.foreachRDD(lambda rdd: classify(rdd))
+    #predictions = process_data(wav_data)
     ssc.start()
     ssc.awaitTermination()
